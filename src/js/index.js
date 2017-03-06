31fbb953a782d46e83a9fc252e46cf04626c028b
Sparrow.jang <sparrow.jang@gmail.com>
2017-03-06 18:03:49 +0800

    force to release global variable

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 
 import frameObserver from './frameObserver';
 export default frameObserver;
+
+if (typeof window !== 'undefined' &&typeof window.frameObserver !== 'undefined') window.frameObserver = frameObserver;
 module.exports = frameObserver;
 
