06eda11538d4c1ae1dd74f3084dad2c2a2cb6382
Sparrow.jang <sparrow.jang@gmail.com>
2018-04-09 10:44:44 +0800

    Release 1.2.2 version

--- dist/frameObserver.js
+++ dist/frameObserver.js
@@ -16,9 +16,9 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	function __webpack_require__(moduleId) {
 /******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-/******/
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
@@ -594,7 +594,7 @@ FrameObserver.prototype = {
     }
 
     //event filter if source is not sended from FrameObserver
-    if (_util2.default.isObject(msgEvt) && _Config.FRAME_OBSERVER in msgEvt) {
+    if (_util2.default.isObject(msgEvt) && msgEvt && _Config.FRAME_OBSERVER in msgEvt) {
       var deferred = (0, _buildDeferred2.default)();
       var source = evt.source;
 
